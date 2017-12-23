0b0fcb20214781155ac563690f447107f20eecda
yechao <best_yc@163.com>
2017-12-23 20:17:44 +0800

    NavAboutActivity

--- app/src/main/java/com/nanchen/aiyagirl/module/navabout/NavAboutActivity.java
+++ app/src/main/java/com/nanchen/aiyagirl/module/navabout/NavAboutActivity.java
@@ -37,34 +37,33 @@ public class NavAboutActivity extends BaseActivity {
                 finish();
             }
         });
-        mTvVersionName.setText("当前版本 V"+PackageUtil.getVersionName());
+        mTvVersionName.setText(String.valueOf("当前版本 V" + PackageUtil.getVersionName()));
     }
-
 
     @OnClick({R.id.tv_new_version, R.id.tv_function, R.id.tv_about_star, R.id.tv_gankio})
     public void onClick(View view) {
+        Intent intent;
         switch (view.getId()) {
             case R.id.tv_new_version:
-                Utils.openLink(this,getResources().getString(R.string.string_url_new_version));
+                Utils.openLink(this, getResources().getString(R.string.string_url_new_version));
                 break;
             case R.id.tv_function:
-//                Utils.openLink(this,getResources().getString(R.string.string_url_other));
-                Intent intentOther = new Intent(this, WebViewActivity.class);
-                intentOther.putExtra(WebViewActivity.GANK_TITLE,"其他开源");
-                intentOther.putExtra(WebViewActivity.GANK_URL,getResources().getString(R.string.string_url_other));
-                startActivity(intentOther);
+                intent = new Intent(this, WebViewActivity.class);
+                intent.putExtra(WebViewActivity.GANK_TITLE, "其他开源");
+                intent.putExtra(WebViewActivity.GANK_URL, getResources().getString(R.string.string_url_other));
+                startActivity(intent);
                 break;
             case R.id.tv_about_star:
-                Intent intent = new Intent(this, WebViewActivity.class);
-                intent.putExtra(WebViewActivity.GANK_TITLE,"给个Star吧");
-                intent.putExtra(WebViewActivity.GANK_URL,getResources().getString(R.string.string_url_AiYaGirl));
+                intent = new Intent(this, WebViewActivity.class);
+                intent.putExtra(WebViewActivity.GANK_TITLE, "给个Star吧");
+                intent.putExtra(WebViewActivity.GANK_URL, getResources().getString(R.string.string_url_AiYaGirl));
                 startActivity(intent);
                 break;
             case R.id.tv_gankio:
-                Intent intent1 = new Intent(this, WebViewActivity.class);
-                intent1.putExtra(WebViewActivity.GANK_TITLE,"拿着Api去玩耍");
-                intent1.putExtra(WebViewActivity.GANK_URL,getResources().getString(R.string.string_url_gankio));
-                startActivity(intent1);
+                intent = new Intent(this, WebViewActivity.class);
+                intent.putExtra(WebViewActivity.GANK_TITLE, "拿着Api去玩耍");
+                intent.putExtra(WebViewActivity.GANK_URL, getResources().getString(R.string.string_url_gankio));
+                startActivity(intent);
                 break;
         }
     }
